61b8c8eaf9056745ac22006bfdbbf7722c620612
MrFrangipane <contact@frangipane.me>
2024-05-20 19:39:10 +0200

    instagram-right + fix overlay amount

--- portfoliogenerator/paragraph.py
+++ portfoliogenerator/paragraph.py
@@ -4,4 +4,13 @@ def make_paragraph(content):
     if isinstance(content, list):
         return '&nbsp;<br/>'.join(content)
 
+    if isinstance(content, dict):
+        lines = content.get('lines', None)
+        if lines is not None:
+            return '&nbsp;<br/>'.join(lines)
+
+        text = content.get('text', None)
+        if text is not None:
+            return text
+
     return content
