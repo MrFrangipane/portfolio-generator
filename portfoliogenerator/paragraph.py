9e21b73c11f102b55f9d6735a7aace57635b8df3
MrFrangipane <contact@frangipane.me>
2024-08-07 15:04:51 +0200

    feat: paragraph.py deals with empty lines (None -> "")

--- portfoliogenerator/paragraph.py
+++ portfoliogenerator/paragraph.py
@@ -2,12 +2,12 @@
 
 def make_paragraph(content):
     if isinstance(content, list):
-        return '&nbsp;<br/>'.join(content)
+        return '&nbsp;<br/>'.join([item if item is not None else "" for item in content])
 
     if isinstance(content, dict):
         lines = content.get('lines', None)
         if lines is not None:
-            return '&nbsp;<br/>'.join(lines)
+            return '&nbsp;<br/>'.join([item if item is not None else "" for item in lines])
 
         text = content.get('text', None)
         if text is not None:
